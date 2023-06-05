ace417bacd0fb95a60fcf9efa5f72f5b7760802a
Alex Georgiev <ageorgiev@unc.edu>
2023-06-05 15:15:21 +0300

    finished vid 12

---
+++ preprocess.py
@@ -0,0 +1,60 @@
+import os
+import librosa 
+import json
+import math
+
+DATASET_PATH = "genre_dataset_reduced"
+JSON_PATH = "data.json"
+SAMPLE_RATE = 22050
+DURATION = 30 # measured in seconds
+SAMPLES_PER_TRACK = SAMPLE_RATE * DURATION
+
+def save_mfcc(dataset_path, json_path, n_mfcc=13, n_fft=2048, hop_length=512, num_segments=5):
+  # dict to store data
+  data = {
+    "mapping": ["classical", "blues"], # classical: 0, blues: 1
+    "mfcc": [], # training inputs
+    "labels": [] # targets
+  }
+
+  num_samples_per_segment = int(SAMPLES_PER_TRACK / num_segments)
+  expected_num_mfcc_vectors_per_segment = math.ceil(num_samples_per_segment / hop_length)
+
+  # loop through all genres
+  for i, (dirpath, dirnames, filenames) in enumerate(os.walk(dataset_path)):
+    # ensure that we're not at the root level
+    if dirpath is not dataset_path:
+      # save semantic label
+      dirpath_components = dirpath.split("/") # genre/blues -> [genre, blues]
+      semantic_label = dirpath_components[-1]
+      data["mapping"].append(semantic_label)
+      print(f"\nProcessoring {semantic_label}")
+
+      # process files for a specific genre
+      for f in filenames:
+        # load audio file
+        file_path = os.path.join(dirpath, f)
+        signal, sr = librosa.load(file_path, sr=SAMPLE_RATE)
+
+        # process segments extracting mfcc and storing data
+        for s in range(num_segments):
+          start_sample = num_samples_per_segment * s
+          finish_sample = start_sample + num_samples_per_segment
+            
+          mfcc = librosa.feature.mfcc(signal[start_sample:finish_sample],
+                                      sr=SAMPLE_RATE,
+                                      n_fft=n_fft,
+                                      n_mfcc=n_mfcc,
+                                      hop_length=hop_length)
+          mfcc = mfcc.T
+
+          # store mfcc for segment if it has the expected length
+          if len(mfcc) == expected_num_mfcc_vectors_per_segment:
+            data["mfcc"].append(mfcc.toList())
+            data["labels"].append(i-1) # first one was data set path, so need to subtract one
+            print("{}, segment: {}".format(file_path, s))
+  with open(json_path, "w") as fp:
+    json.dump(data, fp, index=4)
+
+if __name__ == "__main__":
+  save_mfcc(DATASET_PATH, JSON_PATH, num_segments=10)
